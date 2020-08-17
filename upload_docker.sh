0bbc39abadf907d4ce61bd4ec4813d335c42ec06
Ivan Penchev <ivanpenchev@ue-varna.bg>
2020-08-17 11:03:49 +0200

    figured out the commands to upload to my docker repo

--- upload_docker.sh
+++ upload_docker.sh
@@ -11,6 +11,13 @@ dockerpath="ivanspenchev/udacity-devops"
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+# Idea for improvement
+# this should probably grab all the images by $name
+# iterrate and fetch the tag of the latest on in $imageTag
+# compose $name:$imageTag
+docker image tag ml-prediction-api:latest $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
